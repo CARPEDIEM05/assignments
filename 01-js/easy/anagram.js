83ab015d505f483d227db18893d70b1a15efe4b5
CARPEDIEM05 <99808005+CARPEDIEM05@users.noreply.github.com>
2023-12-04 13:36:27 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,7 +4,27 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+function isAnagram(s, t) {
+  if(s.length!=t.length)return false;
+    let myMap = new Map();
+    for(let i=0;i<s.length;i++){
+        if(!myMap.has(s[i])){
+            myMap.set(s[i],1);
+        }
+        else{
+           myMap.set(s[i],myMap.get(s[i])+1);
+        }
+    }
+    for(let i=0;i<t.length;i++){
+        if(myMap.has(t[i])){
+            myMap.set(t[i],myMap.get(t[i])-1);
+            if(myMap.get(t[i])==0)myMap.delete(t[i]);
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
 
 }
 
