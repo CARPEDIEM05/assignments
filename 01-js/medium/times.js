d0271b7a1bbe9e3551ff923b423e27d1fd03158e
CARPEDIEM05 <99808005+CARPEDIEM05@users.noreply.github.com>
2023-12-04 18:27:52 +0530

    Update times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,9 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let count = 0;
+for(let i=0;i<=10;i++){
+    count +=i;
+}
+console.timeEnd('sumTime');
 }
